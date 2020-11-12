7f384ddbbbec6d5d83b10dbe2d9b8eb568fe03ce
kevinmilet <kevin.milet@gmail.com>
2020-11-12 12:04:54 +0100

    exo 2 ok

--- Ex02/assets/js/script.js
+++ Ex02/assets/js/script.js
@@ -1,9 +1,12 @@
+// on sélection chaque ancre dans le document et pour chaque ancre un ajoute un event listener qui appele la fonction smoothScoll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+    anchor.addEventListener('click', smoothScroll);
 });
+
+// fonction smoothScoll
+function smoothScroll(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
